533d0b0ea69a576f874f1e8140fb999d93a72c7d
samba bery KANE <sambabery.kane@outlook.com>
2018-06-11 14:20:12 +0200

    Mise en place animation des tabs

--- Tabs/js/app.js
+++ Tabs/js/app.js
@@ -1,28 +1,51 @@
 (function(){
     var tabs = document.querySelectorAll('.tabs-link a');
 
-    var afficheContenuOnglet = function (a) {
-        var div = a.parentNode.parentNode.parentNode;
-        var li = a.parentNode;
+    var afficheContenuOnglet = function (a, animations) {
+        if (animations == undefined) {
+            animations = true;
+        }
 
+        var divParent = a.parentNode.parentNode.parentNode;
+        var li = a.parentNode;
+        var activeTab = divParent.querySelector('.tab-content.active');
+        var aAfficher = divParent.querySelector(a.getAttribute('href'));
         if (li.classList.contains('active')) {
             return false;
         }
 
         //On retire la class active sur l'element qui est deja active
-        div.querySelector('.tabs-link .active').classList.remove('active');
+        divParent.querySelector('.tabs-link .active').classList.remove('active');
         //On ajoute la classactive sur le lien actuel
         li.classList.add('active')
 
-        //On retire la classe active au contenu qui actuellement la classe active 
-        div.querySelector('.tab-content.active').classList.remove('active');
 
-        //On ajoute la classe active au contenu en cour
-        div.querySelector(a.getAttribute('href')).classList.add('active');
+        //Effet animation des tabs 
+         var transitionEffect = function () {
+            this.classList.remove('active');
+            this.classList.remove('fade');
+            aAfficher.classList.add('active');
+            aAfficher.classList.add('fade');
+            aAfficher.offsetWith
+            aAfficher.classList.add('in');
+            activeTab.removeEventListener('transitionend', transitionEffect)
+         }
+
+        //affectation de l'affichage 
+        if (animations) {
+            activeTab.classList.add('fade');
+            activeTab.classList.remove('in');
+            activeTab.addEventListener('transitionend', transitionEffect)
+        }else{
+            aAfficher.classList.add('active');
+            activeTab.classList.remove('active');
+        }
     }
+
+
     tabs.forEach(function (tab) {
         tab.addEventListener('click', function (e) {
-            afficheContenuOnglet(this);
+            afficheContenuOnglet(this,true);
         })
     })
 
@@ -30,10 +53,15 @@
      * pour afficher automatique le contenu correcte apres reactaulisation 
      */
 
-    var hash = window.location.hash;
-    var a = document.querySelector('a[href="' + hash + '"]');
-
-    if (a !== null && !a.parentNode.classList.contains('active')) {
-        afficheContenuOnglet(a);
+    var reloadContentOnHashChange = function(e){
+        var hash = window.location.hash;
+        var a = document.querySelector('a[href="' + hash + '"]');
+        if (a !== null && !a.parentNode.classList.contains('active')) {
+            afficheContenuOnglet(a, e !== undefined);
+        }
     }
+
+    window.addEventListener('hashchange', reloadContentOnHashChange);
+    reloadContentOnHashChange();
+
 })()
